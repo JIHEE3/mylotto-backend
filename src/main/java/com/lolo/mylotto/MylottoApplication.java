a462fffa4579e21cc019fb9f1275d672b234e5a2
JIHEE LEE <jihee33777@gmail.com>
2023-10-09 15:49:01 +0900

    초기 셋팅

---
+++ src/main/java/com/lolo/mylotto/MylottoApplication.java
@@ -0,0 +1,13 @@
+package com.lolo.mylotto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MylottoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MylottoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lolo/mylotto/MylottoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lolo.mylotto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MylottoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
